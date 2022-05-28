aff29f54c2316ad7c79bdf9dee7e4b5b3a195ea1
melik baykal <melikbilgeadam@outlook.com>
2022-05-28 17:00:02 +0300

    Add project files.

---
+++ DataAccess/Contexts/ETicaretContext.cs
@@ -0,0 +1,68 @@
+﻿using DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Contexts
+{
+    public class ETicaretContext : DbContext
+    {
+        public DbSet<Urun> Urunler { get; set; }
+        public DbSet<Kategori> Kategoriler { get; set; }
+        public DbSet<Kullanici> Kullanicilar { get; set; }
+        public DbSet<Rol> Roller { get; set; }
+        public DbSet<KullaniciDetayi> KullaniciDetaylari { get; set; }
+        public DbSet<Ulke> Ulkeler { get; set; }
+        public DbSet<Sehir> Sehirler { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // Windows Authentication
+            //string connectionString = "server=.;database=BA_ETicaretCore8523;trusted_connection= true;multipleactiveresultsets=true;";
+
+            // SQL Server Authentication
+            string connectionString = "server=.;database=BA_ETicaretCore8523;user id=sa;password=123;multipleactiveresultsets=true;";
+
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Urun>()
+                //.ToTable("ETicaretUrunler")
+                .HasOne(urun => urun.Kategori)
+                .WithMany(kategori => kategori.Urunler)
+                .HasForeignKey(urun => urun.KategoriId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            modelBuilder.Entity<Kullanici>()
+                //.ToTable("ETicaretKullanicilar")
+                .HasOne(k => k.Rol)
+                .WithMany(r => r.Kullanicilar)
+                .HasForeignKey(k => k.RolId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            modelBuilder.Entity<KullaniciDetayi>()
+                .HasOne(x => x.Kullanici)
+                .WithOne(x => x.KullaniciDetayi)
+                .HasForeignKey<KullaniciDetayi>(x => x.KullaniciId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            modelBuilder.Entity<KullaniciDetayi>()
+               .HasOne(kullaniciDetayi => kullaniciDetayi.Ulke)
+               .WithMany(ulke => ulke.KullaniciDetaylari)
+               .HasForeignKey(kullaniciDetayi => kullaniciDetayi.UlkeId)
+               .OnDelete(DeleteBehavior.NoAction);
+
+            modelBuilder.Entity<KullaniciDetayi>()
+                .HasOne(kullaniciDetayi => kullaniciDetayi.Sehir)
+                .WithMany(sehir => sehir.KullaniciDetaylari)
+                .HasForeignKey(kullaniciDetayi => kullaniciDetayi.SehirId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            modelBuilder.Entity<Sehir>()
+                .HasOne(sehir => sehir.Ulke)
+                .WithMany(ulke => ulke.Sehirler)
+                .HasForeignKey(sehir => sehir.UlkeId)
+                .OnDelete(DeleteBehavior.NoAction);
+        }
+    }
+}
---
+++ MvcWebUI/Controllers/SehirlerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcWebUI.Controllers
+{
+    public class SehirlerController : Controller
+    {
+        public IActionResult SehirlerGet(int ulkeId) // Sehirler/SehirlerGet/1
+        {
+            return null;
+        }
+    }
+}
